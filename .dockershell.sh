612241305d55b98547aa00d00aded65e86beb789
Jon Herrmann <jonherrmann@users.noreply.github.com>
2022-06-04 13:53:31 +0200

    fix antigen

--- .dockershell.sh
+++ .dockershell.sh
@@ -2,7 +2,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-source ~/.antigen/antigen.zsh
+source /home/linuxbrew/.linuxbrew/Cellar/antigen/*/share/antigen/antigen.zsh
 autoload -U colors && colors
 setopt promptsubst
 antigen use oh-my-zsh
