4dd789690ef01c5fe899464308ff0ff214b8148b
Jon Herrmann <jonherrmann@users.noreply.github.com>
2022-06-04 13:15:09 +0200

    init

---
+++ .dockershell.sh
@@ -0,0 +1,16 @@
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+source ~/.antigen/antigen.zsh
+autoload -U colors && colors
+setopt promptsubst
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle docker
+antigen theme cloud
+antigen apply
+
+#bindkey '^H' backward-kill-word
+
+alias kaj="killall -9 java"
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM gitpod/workspace-full
+
+SHELL ["/bin/bash", "-c"]
+USER gitpod
+
+RUN brew install zsh antigen
+RUN curl -s "https://get.sdkman.io" | bash
+ARG JAVA_VERSION="11.0.15-tem"
+RUN . /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk selfupdate force && \
+    yes | sdk install java "${JAVA_VERSION}" && \
+COPY .dockershell.sh ~/.zshrc
+RUN zsh /home/main/.zshrc
