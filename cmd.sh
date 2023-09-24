3a78e2e95aa18c3ce07ce834fa306298670ed83a
J <jonherrmann@users.noreply.github.com>
2023-09-24 22:43:32 +0200

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/home/gitpod/.sdkman/candidates/java/current/bin:/home/gitpod/
 if [ -n "$SBT_CUSTOM_PLUGIN" ]; then
     echo "Installing custom plugin"
     mkdir -p "/home/gitpod/.sbt/1.0/plugins/"
-    echo "$SBT_CUSTOM_PLUGIN" > "/home/gitpod/.sbt/1.0/plugins/custom.scala"
+    echo -e "$SBT_CUSTOM_PLUGIN" > "/home/gitpod/.sbt/1.0/plugins/custom.scala"
 fi
 
 METALS_VERSION="1.0.1"
