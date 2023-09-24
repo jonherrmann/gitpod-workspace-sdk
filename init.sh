c0b61c41b0f88dc7d60e50e96173cca543c9b2ab
J <jonherrmann@users.noreply.github.com>
2023-09-24 22:59:05 +0200

    zsh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 # Install ZSHRC
 cp /home/gitpod/.zshrc.new /home/gitpod/.zshrc
+mkdir -p /home/gitpod/.antigen/bundles/robbyrussell/oh-my-zsh/cache/completions
 
 export PATH=$PATH:/home/gitpod/.sdkman/candidates/java/current/bin:/home/gitpod/.local/share/coursier/bin
 
